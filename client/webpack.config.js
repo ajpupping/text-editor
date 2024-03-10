876d08e15b7707574c634b9b8a54f68dcf404b67
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-10 12:45:56 -0700

    updated webpack config

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -9,22 +9,63 @@ const { InjectManifest } = require('workbox-webpack-plugin');
 module.exports = () => {
   return {
     mode: 'development',
+    // entry point for the application
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
+    // output for the bundled files
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
+    // dev server configuration
     plugins: [
-      
+      // webpack plugin to generate an HTML file
+      new HtmlWebpackPlugin({
+        template: './index.html', 
+        title: 'J.A.T.E.'
+      }), 
+      // webpack plugin to generate a service worker
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'sw.js'
+      }),
+      // webpack plugin to generate a manifest.json file
+      new WebpackPwaManifest({
+        name: 'Just Another Text Editor',
+        short_name: 'J.A.T.E.',
+        description: 'A simple text editor that runs in the browser and works offline.',
+        background_color: '#272822',
+        theme_color: '#31a9e1',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/assets/icon.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('icons'),
+          }
+        ]
+      })     
     ],
 
     module: {
-      rules: [
-        
-      ],
+      // css loaders
+      rules: [{
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      }, 
+      {test: /\.js$/,
+      exclude: /node_modules/,
+      use: {
+        loader: 'babel-loader',
+        options: {
+          presets: ['@babel/preset-env'],
+          plugins: ['@babel/plugin-transform-runtime']}}
+      },
+    
+    ],
     },
   };
 };
