2a9778193358c798af0a2295d0986fd2947fa706
Alyssa <125580811+ajpupping@users.noreply.github.com>
2024-03-10 18:13:55 -0700

    added logic to installation functions, removed todo comments from database

--- client/src/js/install.js
+++ client/src/js/install.js
@@ -1,11 +1,35 @@
 const butInstall = document.getElementById('buttonInstall');
 
+let installPromptEvent;
+
 // Logic for installing the PWA
-// TODO: Add an event handler to the `beforeinstallprompt` event
-window.addEventListener('beforeinstallprompt', (event) => {});
 
-// TODO: Implement a click event handler on the `butInstall` element
-butInstall.addEventListener('click', async () => {});
+// Event handler to the `beforeinstallprompt` event
+window.addEventListener('beforeinstallprompt', (event) => {
+    event.preventDefault();
+    // Stash the event 
+    installPromptEvent = event;
+    // update UI to show install button
+    butInstall.removeAttribute('hidden');
+});
 
-// TODO: Add an handler for the `appinstalled` event
-window.addEventListener('appinstalled', (event) => {});
+// Click event handler on the `butInstall` element
+butInstall.addEventListener('click', async () => {
+    if (installPropmtEvent) {
+        // show the install prompt
+        installPromptEvent.prompt();
+        // wait for user response
+        const { outcome } = await installPromptEvent.userChoice;
+        // clear the prompt event
+        installPromptEvent = null;
+        console.log(`Response: ${outcome}`);
+    }
+});
+
+// Handler for the `appinstalled` event
+window.addEventListener('appinstalled', (event) => {
+    console.log('J.A.T.E. was installed', event);
+    // hide the install button
+    butInstall.setAttribute('hidden', true);
+    window.alert('J.A.T.E. was installed!')
+});
